feb6ab12a51d98778b934f421c71368cc1f44464
ehsan <torabi782@gmail.com>
2024-11-10 18:43:54 +0330

    Add Answer structure Apply Answer struct on parseDNSResponse() and resolveDomain

--- main.go
+++ main.go
@@ -16,6 +16,14 @@ import (
 	"time"
 )
 
+type Answer struct {
+	name  string
+	ip    net.IP
+	ttl   uint32
+	type_ string
+	class string
+}
+
 func buildDNSQuery(domain string) ([]byte, error) {
 	// Transaction ID: random 2 bytes
 	var transactionID uint16 = uint16(0x0000 + rand.Intn(1000))
@@ -46,43 +54,49 @@ func buildDNSQuery(domain string) ([]byte, error) {
 	return append(header, question...), nil
 }
 
-func parseDNSResponse(response []byte) (string, error) {
+func parseDNSResponse(response []byte, domain string) (Answer, error) {
 	//fmt.Printf("respo:\t% x\n", response)
 	ipStart := len(response) - 4
-
+	ttlStart := len(response) - 10
+	classStart := len(response) - 12
+	typeStart := len(response) - 14
 	ip := net.IPv4(response[ipStart], response[ipStart+1], response[ipStart+2], response[ipStart+3])
-	return ip.String(), nil
+	ttl := binary.BigEndian.Uint32(response[ttlStart : ttlStart+4])
+	type_ := response[typeStart : typeStart+2]
+	class := response[classStart : classStart+2]
+	result := Answer{name: domain, ip: ip, ttl: ttl, type_: string(type_), class: string(class)}
+	return result, nil
 }
 
-func resolveDomain(domain string, dnsServer string) (string, error) {
+func resolveDomain(domain string, dnsServer string) (Answer, error) {
 	query, err := buildDNSQuery(domain)
 	//fmt.Printf("query:\t% x\n", query)
 	if err != nil {
-		return "", err
+		return Answer{}, err
 	}
 
 	conn, err := net.Dial("udp", dnsServer+":53")
 	if err != nil {
-		return "", err
+		return Answer{}, err
 	}
 	defer conn.Close()
 
 	err = conn.SetDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
-		return "", err
+		return Answer{}, err
 	}
 	if _, err := conn.Write(query); err != nil {
-		return "", err
+		return Answer{}, err
 	}
 
 	response := make([]byte, 512)
 	read, err := conn.Read(response)
 	if err != nil {
-		return "", err
+		return Answer{}, err
 	}
 
 	// Parse IP address from the response
-	return parseDNSResponse(response[:read])
+	return parseDNSResponse(response[:read], domain)
 }
 
 func main() {
@@ -101,13 +115,13 @@ func main() {
 		for _, s := range domains {
 			s := s
 			go func() {
-				ip, err := resolveDomain(s, dnsServer)
+				answer, err := resolveDomain(s, dnsServer)
 				if err != nil {
 					wg.Done()
 					fmt.Println("Error:", err)
 					return
 				}
-				fmt.Printf("The IP address for %s is %s\n", s, ip)
+				fmt.Printf("The IP address for %s is %s \t TTL: %d  \t type: %s \t class: %s \n", answer.name, answer.ip.String(), answer.ttl, answer.type_, answer.class)
 				wg.Done()
 			}()
 		}
